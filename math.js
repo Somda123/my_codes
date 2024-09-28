2f135b9b342287a1916a2659862fee6b363801bc
Somda123 <162562897+Somda123@users.noreply.github.com>
2024-09-28 17:39:38 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,38 @@
+// ..........random() ( how to work random )
+// console.log(Math.random(1));
+
+// //...........The Math.log() Method
+// Math.log(x) returns the natural logarithm of x.
+// The natural logarithm returns the time needed to reach a certain level of growth:
+// console.log(Math.log(1));
+// console.log(Math.log(0));
+// console.log(Math.log(10));
+// console.log(Math.log(3));
+
+
+// The Math.log2() Method
+// Math.log2(x) returns the base 2 logarithm of x.
+// How many times must we multiply 2 to get 8?
+// console.log(Math.log10(5));
+
+
+// ............
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// for ( let i in fruits)
+// {
+//     console.log(fruits[i]);
+// }
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// for ( let i of  fruits)
+// {
+//     console.log(i);
+// }
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(typeof fruits);
+let a = fruits.toString();
+// console.log(typeof a);
+console.log(a);
